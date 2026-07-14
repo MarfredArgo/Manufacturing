8026f51850189e60a17059262f0ab103e3ec0a06
MarfredArgo <marfredjohn.tano@cvsu.edu.ph>
2026-07-14 12:52:07 +0800

    kaumay na

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,28 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManufacturingController;
 
 // Work orders
-Route::post('/manufacturing/update-order',      [ManufacturingController::class, 'updateOrder']);
+Route::post('/manufacturing/update-order',       [ManufacturingController::class, 'updateOrder']);
 
 // QC benchmark
-Route::post('/manufacturing/update-qc',         [ManufacturingController::class, 'updateQC']);
+Route::post('/manufacturing/update-qc',          [ManufacturingController::class, 'updateQC']);
 
 // Rework
-Route::post('/manufacturing/update-rework',     [ManufacturingController::class, 'updateRework']);
-Route::post('/manufacturing/add-rework-part',   [ManufacturingController::class, 'addReworkPart']);
-Route::post('/manufacturing/update-rework-part',[ManufacturingController::class, 'updateReworkPart']);
+Route::post('/manufacturing/update-rework',      [ManufacturingController::class, 'updateRework']);
+Route::post('/manufacturing/add-rework-part',    [ManufacturingController::class, 'addReworkPart']);
+Route::post('/manufacturing/update-rework-part', [ManufacturingController::class, 'updateReworkPart']);
 
 // Analytics
-Route::post('/manufacturing/add-qc-note',       [ManufacturingController::class, 'addQcNote']);
+Route::post('/manufacturing/add-qc-note',        [ManufacturingController::class, 'addQcNote']);
 
 // Workers
-Route::post('/manufacturing/update-worker',     [ManufacturingController::class, 'updateWorker']);
-Route::post('/manufacturing/delete-worker',     [ManufacturingController::class, 'deleteWorker']);
-Route::post('/workorder/assignment',            [ManufacturingController::class, 'addWorker']);
-Route::post('/workorder/assign-worker',         [ManufacturingController::class, 'assignWorker']);
+Route::post('/manufacturing/update-worker',      [ManufacturingController::class, 'updateWorker']);
+Route::post('/manufacturing/delete-worker',      [ManufacturingController::class, 'deleteWorker']);
+Route::post('/workorder/assignment',             [ManufacturingController::class, 'addWorker']);
+Route::post('/workorder/assign-worker',          [ManufacturingController::class, 'assignWorker']);
 
 // Pages
-Route::get('/manufacturing', fn() => view('Manufacturing'));
+Route::get('/',              [ManufacturingController::class, 'index']);
+Route::get('/manufacturing', [ManufacturingController::class, 'index']);
 Route::get('/welcome',       fn() => view('welcome'));
 Route::get('/contactus',     fn() => view('Contactus'));
 Route::get('/signin',        fn() => view('Signin'));
-Route::get('/',              fn() => view('Manufacturing'));
