bd98d88e06b70da24c25ce06f2d71eb82137be43
Marcaux-LUUUVS-Femboys <jeanmarcaux.lomangaya@cvsu.edu.ph>
2026-07-15 13:50:23 +0800

    07/15/26 13:50

--- config/database.php
+++ config/database.php
@@ -85,14 +85,21 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL') ?? null,
-            'port' => env('DB_PORT'),
-            'charset' => 'utf8',
-            'prefix' => '',
+            'driver'         => 'pgsql',
+            'host'           => env('DB_HOST', '127.0.0.1'),
+            'port'           => env('DB_PORT', '5432'),
+            'database'       => env('DB_DATABASE', 'manufacturing'),
+            'username'       => env('DB_USERNAME', 'postgres'),
+            'password'       => env('DB_PASSWORD', ''),
+            'charset'        => 'utf8',
+            'prefix'         => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'require',
+            'search_path'    => 'public',
+            'sslmode'        => env('DB_SSLMODE', 'require'),
+            'options'        => array_filter([
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+                'endpoint'                        => env('DB_OPTIONS'),
+            ]),
         ],
 
         'sqlsrv' => [
@@ -112,32 +119,10 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
-    */
-
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => true,
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
-    */
 
     'redis' => [
 
